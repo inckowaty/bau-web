ab30f7d0a9d17d18ae94c5268658cc6dfdf9d00c
Adam Adamczyk <adamczykeu@gmail.com>
2025-06-30 11:57:47 +0200

    Big Update v2

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,13 +1,14 @@
 import styles from "./Footer.module.css";
+import Link from 'next/link';
 
 export default function Footer() {
   return (
     <footer className={styles.footer}>
       <p>Bau GmbH © {new Date().getFullYear()} • USt-IdNr DE-123456789</p>
       <p>
-        <a href="/impressum">Impressum</a>
+        <Link href="/impressum">Impressum</Link>
         <span style={{ margin: "0 0.5rem" }}>•</span>
-        <a href="/datenschutz">Datenschutz</a>
+        <Link href="/datenschutz">Datenschutz</Link>
       </p>
     </footer>
   );
