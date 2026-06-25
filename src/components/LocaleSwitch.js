590727f7435e1be26260766849e252f00482f446
inckowaty <adamczykeu@gmail.com>
2026-06-25 14:54:12 +0200

    Fix CS and ESLint warnings, remove prisma config

--- src/components/LocaleSwitch.js
+++ src/components/LocaleSwitch.js
@@ -1,5 +1,7 @@
 'use client';
 
+import Image from 'next/image';
+
 const icons = {
   de: '/flags/de.svg',
   pl: '/flags/pl.svg',
@@ -13,7 +15,7 @@ export default function LocaleSwitch({langUrls}) {
     <div style={{display:'flex',gap:'.5rem',marginRight:'auto'}}>
       {Object.entries(langUrls).map(([l,url])=>(
         <a key={l} href={url}>
-          <img src={icons[l]} alt={l} width={24} height={16}/>
+          <Image src={icons[l]} alt={l} width={24} height={16}/>
         </a>
       ))}
     </div>
